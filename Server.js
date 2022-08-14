8b31e962c9be0bc6da638a9d9e0153f4521c8323
Sahil <sahilrohera10@gmail.com>
2022-08-14 13:07:11 +0530

    backend 1st commit

---
+++ Server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 3222;
+
+const cors = require("cors");
+dotenv.config();
+
+//CONNECTING MONGODB
+
+// mongoose
+//   .connect(process.env.DB_CONNECT, {})
+//   .then(() => console.log("Database connected!"))
+//   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
+// const Routes = require("./Routes/apiRoutes");
+// app.use("/", Routes);
+
+app.listen(PORT, () => {
+  console.log("server started and running on port 3222");
+});
